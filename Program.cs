93b7f668dca46c885b87a6a4b89454963bccf6f5
Sergey <Sergey@DESKTOP-VV7LR7L>
2022-07-25 22:19:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Привет, как тебя зовут?");
+            string Name = Console.ReadLine();
+            Console.WriteLine(Name);
+            Console.ReadKey();
+
+        }
+    }
+}
